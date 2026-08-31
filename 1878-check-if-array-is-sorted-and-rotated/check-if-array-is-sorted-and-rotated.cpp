bebd660c61e3c0bfdcca0476e6464d51bd7caaa3
Biki Borah <bikiborah896@gmail.com>
2026-09-01 00:21:14 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (19.34%) - LeetSync

---
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int drop=0;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]<nums[i-1]){
+                drop++;
+            }
+        }
+        if(nums[0]<nums[nums.size()-1]){
+            drop++;
+        }
+        if(drop<=1){
+            return true;
+        }
+        return false;
+    }
+};
