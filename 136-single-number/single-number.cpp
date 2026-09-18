b4a3e84e9f17f75244735955531ef51738776a84
Biki Borah <bikiborah896@gmail.com>
2026-09-18 23:37:34 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (98.71%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        map<int,int> mpp;
-        int x=0;
-        for(int i=0;i<nums.size();i++){
-            mpp[nums[i]]++;
+        int ans=0;
+        for(auto x:nums){
+            ans=ans^x;
         }
-        for(int i=0;i<nums.size();i++){
-            if(mpp[nums[i]]==1){
-                x=nums[i];
-            }
-        }
-        return x;
+        return ans;
     }
 };
